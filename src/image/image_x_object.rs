9d63f3af77279c6069e3f21fdea982c3641cff1b
Danik Vitek <x3665107@gmail.com>
2023-08-25 23:55:56 +0300

    Improve readability and use of newtype pattern

--- src/image/image_x_object.rs
+++ src/image/image_x_object.rs
@@ -1,17 +1,10 @@
 use printpdf::{scale::Px, xobject::ImageXObject, Mm};
 
-fn pixel_to_mm(pixel: usize) -> Mm {
+fn pixel_to_mm(Px(pixel): Px) -> Mm {
     Mm((pixel as f64) * 0.084666667)
 }
 
 pub fn get_image_dimension_in_mm(image_object: &ImageXObject) -> (Mm, Mm) {
-    let ImageXObject {
-        width: Px(image_width_in_px),
-        height: Px(image_height_in_px),
-        ..
-    } = image_object;
-
-    let image_width = pixel_to_mm(*image_width_in_px);
-    let image_height = pixel_to_mm(*image_height_in_px);
-    (image_width, image_height)
+    let ImageXObject { width, height, .. } = image_object;
+    (pixel_to_mm(*width), pixel_to_mm(*height))
 }
